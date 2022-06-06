b52faf38ec5947cd031568e101a71bb245e91a7b
jelmer <geojelmer@gmail.com>
2022-06-06 22:43:18 +0200

    breaking it down

--- app/Example.hs
+++ app/Example.hs
@@ -7,35 +7,9 @@ type Model = [Picture -> Picture]
 fractal :: Int -> Model -> Picture -> Picture
 fractal n model pict = pictures $ take n $ iterate (mconcat model) pict
 
-{-
-        iterate (mconcat model)
-        - itereert over elk item in model 
-        - begint met pict als start item
-
-        take n
-        - pakt x aantal van de array die iterate produceert
-        - hierdoor zorg je dat de fractal tree niet oneindig doorgaat
-
-        pictures
-        - Model [Picture -> Picture] dat de voorgaande functies produceren
--}
- 
-
 pithagor _ = fractal 10 model $ rectangleWire 100 100
   where model = [ Translate 50 100 . Scale s s . Rotate 45
                 , Translate (-50) 100 . Scale s s . Rotate (-45)]
         s = 1/sqrt 2
  
 main = animate (InWindow "Tree" (800, 800) (0, 0)) white $ pithagor . (* 60)
-
-{-
-        InWindow
-        - arg1 : naam van window
-        - arg2 : groote van window
-        - arg3 : reference points van window op scherm       
--}
-
-
-
-
- 
--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,41 @@ import Graphics.Gloss
 
 type Model = [Picture -> Picture]
 
+algoScale :: Float
+algoScale = 1/sqrt 2
 
-testRect :: Float -> Float -> Picture
-testRect x y = rectangleWire x y
+algorithm :: Model 
+algorithm = [ Translate 50 100 . Scale algoScale algoScale . Rotate 45, Translate (-50) 100 . Scale algoScale algoScale . Rotate (-45) ]  
 
-pic1 :: Picture
-pic1 = testRect 100 100 
+fractal :: Int -> Model -> Picture -> Picture
+fractal n model pict = pictures (take n ( iterate (mconcat model) pict))
+{-
+    fractal
+    - arg1 : max n of iterations on algorithm
+    - arg2 : model in its entirety
+    - arg3 : gloss 2d object to use for algorithm visual
 
-pic2 :: Picture
-pic2 = testRect 200 200
+    pictures 
+    - [Picture] -> Picture
+-}
 
-pictures :: Model 
-pictures = [pic1, pic2]
+pithagor = fractal 10 algorithm $ rectangleWire 100 100
+{-
+    Calls the fractal function with max 10 iterations on the algorithm.
+    Uses a rectangle of 100x100 px as the 2D object for the visual.   
+-}
 
+main = display (InWindow "Fractal Tree" (800, 800) (10, 10)) black $ Color white pithagor
+{-
 
+    display
+    - arg1 : window type
+    - arg2 : window background color
+    - arg3 : picture to display in window
 
-pitaghoras :: Picture
-pitaghoras = rectangleWire 100 100
+    InWindow
+    - arg1 : name of window
+    - arg2 : size of window
+    - arg3 : position of window on screen
 
-main = display (InWindow "Fractal Tree" (800, 800) (10, 10)) black (Color white (pitaghoras))
+-}
