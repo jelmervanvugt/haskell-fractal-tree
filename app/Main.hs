78ef422d4ca219b1be6aea12b78357d3b7b8366d
jelmer <geojelmer@gmail.com>
2022-06-07 21:02:37 +0200

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,9 @@
+{-
+    Author: Jelmer van Vugt
+    Date: 07/06/2022
+    PO Paradigma - APP, Hogeschool van Arnhem en Nijmegen
+-}
+
 module Main where
 
 import Graphics.Gloss
@@ -5,15 +11,15 @@ import Graphics.Gloss
 type Model = [Picture -> Picture]
 
 algoScale :: Float
-algoScale = 1/sqrt 2
+algoScale = 1 / sqrt 2
 
-algorithm :: Model 
-algorithm = [ Translate 50 100 . Scale algoScale algoScale . Rotate 45, Translate (-50) 100 . Scale algoScale algoScale . Rotate (-45) ]  
+fractalAlgorithm :: Model 
+fractalAlgorithm = [ Translate 50 100 . Scale algoScale algoScale . Rotate 45, Translate (-50) 100 . Scale algoScale algoScale . Rotate (-45) ]  
 {-
     algoScale
     - variable that calculates how much the rect needs to downsize each iteration
 
-    algorithm
+    fractalAlgorithm
     - algorithm that'll result in a Pythagoras' fractal tree
 -}
 
@@ -39,18 +45,19 @@ fractal max model pict = pictures $ applyAlgorithm max model pict
     - appends array of pictures into one, enabling us to use the display function
 -}
 
-pithagor :: Float -> Picture
-pithagor time 
-  | (ceiling time) `mod` 2 == 0 = Color white $ fractal 10 algorithm $ rectangleWire 100 100
-  | otherwise = Color blue $ fractal 10 algorithm $ rectangleWire 100 100
+pythagor :: Float -> Picture
+pythagor time 
+  | (ceiling time) `mod` 2 == 0 = Color white tree
+  | otherwise = Color blue tree
+  where tree = fractal 10 fractalAlgorithm $ rectangleWire 100 100
 {-
     calls the fractal function with max 10 iterations on the algorithm.
     uses a rectangle of 100x100 px as the 2D object for the visual.   
     
-    pithagor changes fractal tree color based on time variable passed by animate in main function
+    pythagor changes fractal tree color based on time variable passed by animate in main function
 -}
 
-main = animate (InWindow "Fractal Tree" (800, 800) (10, 10)) black $ pithagor
+main = animate (InWindow "Fractal Tree" (800, 800) (10, 10)) black $ pythagor 
 {-
     animate
     - arg1 : window type
